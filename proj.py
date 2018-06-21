46acd82197331547020b39fc16be18a921069244
aexms <33584937+aexms@users.noreply.github.com>
2018-06-21 23:00:25 +0300

    Update proj.py

--- proj.py
+++ proj.py
@@ -24,11 +24,18 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 # Creating creating temporary directory for frames
+
 workdir = './frames_' + os.path.splitext(os.path.basename(argv[1]))[0]
 if not os.path.exists(workdir):
     os.mkdir(workdir)
-os.system("ffmpeg -i " + argv[1] + ".mp4 -vf scale=320:240 " + argv[1] + "1.mp4")
-os.system("ffmpeg -i " + argv[1] + '1.mp4 ' + workdir + '/frame%d.png')
+
+# Rescaling video
+os.system("ffmpeg -i " + argv[1] + " -vf scale=320:240 ./sm_" + os.path.splitext(os.path.basename(argv[1]))[0] +
+          os.path.splitext(os.path.basename(argv[1]))[1])
+
+# Extracting frames
+os.system("ffmpeg -i ./sm_" + os.path.splitext(os.path.basename(argv[1]))[0] +
+          os.path.splitext(os.path.basename(argv[1]))[1] + ' ' + workdir + '/frame%d.png')
 
 # Clustering
 def backgr(frms):
@@ -41,6 +48,8 @@ def backgr(frms):
             db = DBSCAN(eps=4, min_samples=4).fit(pixels)
             labels = db.labels_
             label_set = set(labels)
+            # if len(label_set) > 2:
+            #    print(len(label_set), i, j)
             lbcnt = {k: 0 for k in label_set}
             for k in labels:
                 lbcnt[k] += 1
@@ -50,14 +59,13 @@ def backgr(frms):
                     choice = k            
             if choice != -1:
                 row.append(np.mean(np.array([pixels[k] for k in range(len(labels)) if labels[k] == choice]), axis=0))
-                # Painting not background pixels
                 frms[:, i, j, 0] -= frms[:, i, j, 0] * (labels == -1)
                 frms[:, i, j, 2] -= frms[:, i, j, 2] * (labels == -1)
             else:
                 row.append(np.mean(pixels, axis=0))
         image.append(row)
-    return image
 
+    return image
 
 frames = np.array([np.asarray(Image.open(os.path.join(workdir, 'frame' + str(f + 1) + '.png')))
                    for f in range(len(os.listdir(workdir)))])
@@ -67,19 +75,17 @@ k = 0
 prev = 0
 res = []
 term = []
+
 # Scene change detection
 for i in range(1, len(os.listdir(workdir))):
     image2 = frames[i]
     img1 = rgb2gray(image1)
     img2 = rgb2gray(image2)
-    if sum((img1 - img2)**2) > 750:
+    if sum((img1 - img2)**2) > 5000:
         k += 1
         if i - prev > 10:
             if k <= 7:
-                print(sum((img1 - img2)**2), i)
-            # ans = backgr(frames[prev:min(i-1, prev + 100)])
             ans = backgr(frames[prev:i - 1])
-            # ans = frames[prev]
             ans = np.array(ans).astype(np.uint8)
             res.append(ans)
             term.append(prev)
@@ -89,53 +95,63 @@ print(k)
 term.append(prev)
 term.append(len(os.listdir(workdir)))
 
+
+# ans = backgr(frames[prev:min(prev + 100, len(os.listdir(workdir)))])
 ans = backgr(frames[prev:len(os.listdir(workdir))])
-ans = frames[prev]
-ans = np.array(frames[0]).astype(np.uint8)
+ans = np.array(ans).astype(np.uint8)
 res.append(ans)
 
 res = np.array(res).astype(np.uint8)
 cpy = np.copy(res[0])
 
 term = np.array(term)
-frames1 = np.copy(frames)
-frames1 = np.array(frames1).astype(np.uint8)
+
+frames = np.array(frames).astype(np.uint8)
+# print(term.shape, term[0], term[1], len(os.listdir(workdir)))
+# print(term)
+# print(res.shape)
 
 # Searching for the same locations
 loc = np.arange(res.shape[0])
 for i in range(loc.shape[0]):
     if i == 0 or loc[i] == i:
+        # print(i)
         if i != 0 and loc[i] == i:
             loc[i] = loc[i-1] + 1
         img1 = rgb2gray(res[i])
         for j in range(i + 1, loc.shape[0]):
             img2 = rgb2gray(res[j])
-            if sum((img1 - img2) ** 2) < 1500:
+            # print(i, j, sum((img1 - img2) ** 2))
+            if sum((img1 - img2) ** 2) < 5000:
                 loc[j] = loc[i]
+# print(loc)
 
 outdir = './loc1_' + os.path.splitext(os.path.basename(argv[1]))[0]
 if not os.path.exists(outdir):
     os.mkdir(outdir)
+
 # Adding location number to frames and saving
 for i in range(1, term.shape[0]):
     for j in range(term[i - 1], term[i]):
-        imsave(outdir + '/loc' + str(j + 1) + '.png', frames1[j])
+        imsave(outdir + '/loc' + str(j + 1) + '.png', frames[j])
         img = Image.open(outdir + '/loc' + str(j + 1) + '.png')
         draw = ImageDraw.Draw(img)
-        draw.text((0, 0), "location " + str(loc[i-1]), (0, 0, 255))
+        draw.text((0, 0), "location " + str(loc[i-1] + 1), (0, 255, 255))
         img.save(outdir + '/loc' + str(j + 1) + '.png')
         
 for i in range(res.shape[0]):
     imsave(outdir + '/locrs' + str(i + 1) + '.png', res[i])
 
 # Creating final video
-if not argv[2]:
-    outpath = './movie'
+if len(argv) < 3:
+    outpath = './movie.mp4'
 else:
     outpath = argv[2]
-os.system("ffmpeg -r 30 -i " + outdir + "/loc%d.png -vcodec mpeg4 -y " + outpath + ".mp4")
+os.system("ffmpeg -r 30 -i " + outdir + "/loc%d.png -vcodec mpeg4 -y " + outpath)
+# os.system("ffmpeg -r 30 -i E:/frames_inpt5/frame%d.png -vcodec mpeg4 -y E:/inpt5.mp4")
 
 # Deleting files
+os.remove("./sm_" + os.path.splitext(os.path.basename(argv[1]))[0] + os.path.splitext(os.path.basename(argv[1]))[1])
 for f in os.listdir(workdir):
     os.remove(os.path.join(workdir, f))
 os.rmdir(workdir)
